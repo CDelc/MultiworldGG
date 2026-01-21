3bb27b7efc156822f2abb3eac1d88d60990d422b
TreZc0_ <trezc0@gmx.de>
2026-01-21 20:35:10 +0100

    Merge upstream

--- Utils.py
+++ Utils.py
@@ -52,7 +52,7 @@ class Version(typing.NamedTuple):
         return ".".join(str(item) for item in self)
 
 
-__version__ = "0.6.6"
+__version__ = "0.6.7"
 version_tuple = tuplize_version(__version__)
 
 instance_name = "MultiworldGG"
